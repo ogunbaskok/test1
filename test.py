4e5995988221439ddc32a536c14f3ed225619a92
ogün <ogunbaskok52@gmail.com>
2022-02-08 04:18:05 +0300

    t

---
+++ test.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+PATH = "C:\Program Files\chromedriver\chromedriver1.exe"
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://google.com.tr")
+time.sleep(5)
+driver.maximize_window()
+a = 5
+b = 7 
+
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+print("basari ile çalistirildi",a+b)
+
+time.sleep(5)
+    
+   
