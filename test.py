c7e99d177b3ce282742cdd31367b5ac2f0b8bea7
ogün <ogunbaskok52@gmail.com>
2022-02-09 23:48:29 +0300

    9subat

--- test.py
+++ test.py
@@ -6,50 +6,33 @@ import time
 PATH = "C:\Program Files\chromedriver\chromedriver1.exe"
 
 driver = webdriver.Chrome(PATH)
-
-driver.get("https://google.com.tr")
-time.sleep(5)
 driver.maximize_window()
+driver.get("https://google.com.tr")
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys("trabzonspor")
+driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys(Keys.ENTER)
+
+deger = driver.find_element_by_xpath("//*[@id='kp-wp-tab-overview']/div[1]/div/div/div/div/div/div[2]/div/div/div/span[2]").text
+time.sleep(3)
+
+
 a = 5
 b = 7 
 
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
-print("basari ile çalistirildi",a+b)
+time.sleep(3)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+print(deger)
+
 
 time.sleep(5)
     
